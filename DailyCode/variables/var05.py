a31e9b78cc58c9cb539f6904d89a5dfbce8d464d
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-01-21 23:57:20 +0530

    new var problem added

--- DailyCode/variables/var05.py
+++ DailyCode/variables/var05.py
@@ -1,1 +1,2 @@
-print("The length of the strng is", len(input("Enter the string\n")) )
+print("The length of the string is", len(input("Enter the string\n")) )
+#took a bit of time to figure out this one,the instructor gave it as a challenge.
---
+++ DailyCode/variables/var06.py
@@ -0,0 +1,3 @@
+username = input("Ener the string\n")
+Length = len(username)
+print("The Length is", Length)
