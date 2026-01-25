e31ec2d19f786e5891fcad81f6556c1ac4c10307
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-01-25 22:40:46 +0530

    new prob here

---
+++ DailyCode/Statements/State01.py
@@ -0,0 +1,6 @@
+print("Welcome to the Roller Coaster!")
+Height = int(input("What's your height in cm?\n"))
+if Height>=120:
+   print("You can ride the roller coster")
+else:
+   print("You can't ride the roller coster") #sorry shorty lol   
