fdc001c3d205ee13d8209b84fc2c5f980551eb90
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-01-26 20:23:20 +0530

    New challenge

---
+++ Mini_Challenges/Pizaa_order.py
@@ -0,0 +1,22 @@
+print("Welcome to the pizza deliveries!")
+size = input("What size pizza would you like? S,M or L:\n")
+pepperoni = input("Would you like peperoni on your pizza Y or N:\n")
+cheese =  input("Do you want extra cheese on your pizza? Y OR N:\n")
+Bill = 0
+
+if size=="L" :
+    Bill += 25
+    if pepperoni == "Y":
+        Bill += 3
+elif size == "M" :
+    Bill += 20
+    if pepperoni == "Y":
+        Bill += 3
+else:
+    Bill += 15
+    if pepperoni == "Y":
+        Bill += 2
+if cheese=="Y":
+     Bill += 1
+
+print(f"Thanks for ordering,your total bill is: ${Bill}")
