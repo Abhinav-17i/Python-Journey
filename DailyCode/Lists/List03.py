a65002d697917e02d80552fbfc1b3bcf228deab4
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-01-28 16:33:05 +0530

    New prob

---
+++ DailyCode/Lists/List03.py
@@ -0,0 +1,11 @@
+Fruits = ["Apple","Mango","Banana","pineapple","Grapes"]
+
+Fruits[3] = "Orange"
+
+print(Fruits)
+
+Fruits.append("Watermelon") 
+print(Fruits)
+
+Fruits.extend(["Pear","Kiwi"])
+print(Fruits)
