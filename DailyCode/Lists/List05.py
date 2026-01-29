1aa6a40742172be3895f351358a0fb2a8683f9b0
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-01-29 15:29:32 +0530

    new probpython -u

---
+++ DailyCode/Lists/List05.py
@@ -0,0 +1,6 @@
+vegetables = ["Potato","Tomato","Spinach","Cauliflower","Cucumber","Carrot"]
+Fruits = ["Banana","Apple","Pineapple","Mango","Cherry"]
+
+Combined = [vegetables,Fruits]
+
+print(Combined[1][-1])
