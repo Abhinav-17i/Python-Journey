f8d4bca234375c7b4e0cb5bfc68858695769814f
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-01-30 22:55:24 +0530

    new prob

--- DailyCode/Loops/Loop03.py
+++ DailyCode/Loops/Loop03.py
@@ -2,6 +2,7 @@ List_score  = (10,20,30,40,50,60,70,80,90,100)
 
 total_score = sum(List_score)
 
-print(total_score) 
-
-#new key for sum of  lists or no.
+sum = 0
+for score in List_score :
+    sum += score
+    print(sum)
