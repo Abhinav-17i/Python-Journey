679fa3db7d6419b9c8cb2390891437cb0c9a26dc
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-01-31 22:54:03 +0530

    Second Que Done

---
+++ DailyCode/Practice_Questions/SumOfList.py
@@ -0,0 +1,12 @@
+# Sum of list elements without using sum()
+List = [10,20,30,40,50]
+
+n = len(List)
+
+total = 0
+for i in List :
+    total += i
+
+print(f"The sum of the list elements is {total}")
+# m = sum(List)
+# print(m)
