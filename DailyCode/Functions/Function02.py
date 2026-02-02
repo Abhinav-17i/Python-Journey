30ea769394df89c8c475083e58316e6d1348bea8
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-02-02 20:36:34 +0530

    7th Que completed

---
+++ DailyCode/Functions/Function02.py
@@ -0,0 +1,31 @@
+#Reboorgs world hurdle 4 solution
+#check out this link:- https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+#solution :-
+# def turnright():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+     
+# def foal():
+#     turn_left()
+#     move()
+#     turnright()
+#     move()
+#     turnright()
+#     move()
+#     turn_left()
+    
+# while at_goal()!= True :
+#     if right_is_clear():
+#         turnright()
+#         move()
+#     else :   
+#         if wall_in_front() :
+#             turn_left()
+#         else :
+#             move()
+    
+    
+    
+
+    
