3d3380007973c71a5a520b0b7190472a7e1986d1
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-02-04 20:26:43 +0530

    Reboorg's world maze solution

---
+++ DailyCode/Functions/Function04.py
@@ -0,0 +1,25 @@
+# reeborgs world linkhttps://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+# reboorgs world maze solution
+
+# def turnright():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+     
+# def foal():
+#     if right_is_clear():
+#         turnright()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     elif wall_on_right():
+#         turn_left()
+#     else :
+#         turnright()
+          
+# while at_goal() != True :
+#     foal()
+    
+   
+    
+
