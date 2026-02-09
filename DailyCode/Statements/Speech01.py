d5bf01a3c5818deca8963d5a465e6cacb66ea812
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-02-09 22:46:12 +0530

    speed run

---
+++ DailyCode/Statements/Speech01.py
@@ -0,0 +1,12 @@
+speech = """
+Peace is nothing more than a pause between battles.
+Every generation fights for its own definition of freedom.
+Laws are written by those who hold authority,
+and broken by those desperate enough to resist it.
+Right and wrong shift like shadows,
+depending on where the light falls.
+And when the smoke clears,
+only the winners decide what was righteous.
+"""
+
+print(speech)
