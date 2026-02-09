fd4408c2a6f6ebb1a1dd3edeb269bf30f4055720
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-02-09 22:48:20 +0530

    speed run

---
+++ DailyCode/Statements/Speech02.py
@@ -0,0 +1,12 @@
+speech = """
+Heroes and villains are decided by the stories people choose to believe.
+In times of chaos, truth is buried beneath victory.
+The strong call their actions justice,
+while the weak call the same acts cruelty.
+Power reshapes morality,
+and history remembers only the side that survives.
+In the end, the world kneels not to justice,
+but to the will of the victor.
+"""
+
+print(speech)
