d5c6c7e9fe66ce6f5748b14ff2f75ea114940e45
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-02-10 22:21:54 +0530

    new concept

---
+++ DailyCode/Functions/Function05.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    print("Sum:", a + b)
+
+def subtract(a, b):
+    print("Difference:", a - b)
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+add(x, y)
+subtract(x, y)
