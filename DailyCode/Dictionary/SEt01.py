ed4a6b6212d8916cf224290ace95db086383a8d2
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-02-23 21:00:14 +0530

    correction and learning

--- DailyCode/Dictionary/SEt01.py
+++ DailyCode/Dictionary/SEt01.py
@@ -1,4 +1,4 @@
-set1 = (1,2,2,3,4,5,"rollin")
+set1 = {1,2,2,3,4,5,"rollin"}
 
 print(set1)
 
---
+++ DailyCode/Dictionary/Set02.py
@@ -0,0 +1,7 @@
+set1 = {1,2,2,3,4,5,"rollin",5,7,8,9,9}
+
+set1.remove(1)
+print(set1)
+
+set1.add(1)
+print(set1)
