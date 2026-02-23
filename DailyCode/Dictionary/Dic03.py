45ec503bbd8b131a5ec21cb578a8f5ad9bc447e0
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-02-23 20:34:38 +0530

    NEW PROB

---
+++ DailyCode/Dictionary/Dic03.py
@@ -0,0 +1,13 @@
+dic = {
+    "Name" : "Robert Terravox",
+    "Age": 27,
+    "Marks": {
+       "Maths" : 97,
+       "Physics" : 98,
+       "Chemisry" : 92
+    }   
+}
+
+insert = {"Name": "Murthy","Age": 25,"Marks" : {"maths":85,"Physics":78,"Chemistry":75}}
+dic.update(insert)
+print(dic)
