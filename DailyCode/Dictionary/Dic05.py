3166e47a4a52f8cdf8bf8f990fddac80c7efb383
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-02-23 20:50:38 +0530

    new prob

---
+++ DailyCode/Dictionary/Dic05.py
@@ -0,0 +1,13 @@
+dic = {
+    "Name" : "Robert Terravox",
+    "Age": 27,
+    "Marks": {
+       "Maths" : 97,
+       "Physics" : 98,
+       "Chemisry" : 92
+    }   
+}
+
+print(dic.get("Name1")) #this will nt give error
+
+#print(dic["Name1"]) this one give error
