1d18032872bab1987fa8fc025ccbf0a643fe3929
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-02-24 19:11:26 +0530

    news

---
+++ DailyCode/Dictionary/Dic06.py
@@ -0,0 +1,11 @@
+student = {
+    "name": "Abhinav",
+    "age": 20,
+    "course": "Python"
+}
+
+print("Student Name:", student["name"])
+
+print("Age:", student["age"])
+
+print("Course:", student["course"])
