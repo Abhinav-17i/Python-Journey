20008046d211f7b5e3d74bdb3aa5c8aff3b37f9b
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-03-02 00:16:23 +0530

    prob done

---
+++ DailyCode/Break&Continue/Br02.py
@@ -0,0 +1,12 @@
+list = [2,4,9,5,8,6,1,7]
+
+n = int(input("Enter the number you want to find: "))
+
+for i in range (0,len(list)) :
+    if list[i] == n :
+        print(f"The entered number found at index {i}")
+        break
+
+else :
+    print("Not Found")
+
