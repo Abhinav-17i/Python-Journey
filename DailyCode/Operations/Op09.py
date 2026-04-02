835e1c673656a20777f0a24ce0df14362c924066
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-04-02 21:55:07 +0530

    membership added

--- DailyCode/Operations/Op09.py
+++ DailyCode/Operations/Op09.py
@@ -1,14 +1,12 @@
-# Smart Discount Calculator
-
 amount = float(input("Enter total purchase amount: "))
 is_member = input("Are you a member? (yes/no): ").lower()
 
 discount = 0.2 if amount > 1000 else 0.1 if amount > 500 else 0
 
-# Extra 5% for members
 discount += 0.05 if is_member == "yes" else 0
 
 final_price = amount - (amount * discount)
 
 print("\nDiscount Applied:", discount * 100, "%")
+
 print("Final Price to Pay:", final_price)
