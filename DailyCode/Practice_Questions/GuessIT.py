54d11fcb1f56ddba5b260ec703ebfd9f273db367
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-04-08 20:26:51 +0530

    guessing

--- DailyCode/Practice_Questions/GuessIT.py
+++ DailyCode/Practice_Questions/GuessIT.py
@@ -1,10 +1,9 @@
 password = "python123"
 guess = ""
-
 while guess != password:
     guess = input("Enter the secret password: ")
     
     if guess != password:
         print("Wrong password! Try again bro!!")
-
+        
 print("Access Granted! Welcome Hacker !!")
