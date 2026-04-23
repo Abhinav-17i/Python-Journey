1a4aa3007f761ad09dd862472dfe803471c53bbf
Abhinav Bisen <abhinavbisen18@gmail.com>
2026-04-23 20:37:03 +0530

    diff ap

--- DailyCode/Practice_Questions/EvenOdd_counter.py
+++ DailyCode/Practice_Questions/EvenOdd_counter.py
@@ -1,5 +1,6 @@
 #This have to enter a number 'n' and this code will figure out how many even and odd numbers are there from 1to 'n'
 print("Welcome to even & odd counter this code will guess how many even and odd numbers are there from 1 to any number you enter.")
+
 n = int(input("Enter a number :\n"))
 
 even = 0
@@ -12,6 +13,7 @@ for i in range(1,n + 1) :
         odd +=1
 
 print(f"There are {even} even numbers from 1 to {n}.")
+
 print(f"There are {odd} odd numbers from 1 to {n}.")
 
         
