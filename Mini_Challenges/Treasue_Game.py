dfd18fa349d071433373d163d6953ac747d2ed19
atharvthadve <atharvthadve08@gmail.com>
2026-09-01 00:39:18 +0530

    Refactor Treasure Game logic and improve readability

--- Mini_Challenges/Treasue_Game.py
+++ Mini_Challenges/Treasue_Game.py
@@ -5,6 +5,8 @@ print('''***********************************************************************
 |___________________|__"=._o`"-._        `"=.______________|___________________
           |                `"=._o`"=._      _`"=._                     |
  _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
 |                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
 |___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
           |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
@@ -12,29 +14,48 @@ print('''***********************************************************************
 |                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
 |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
 ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______
 ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/____
 /______/______/______/______/______/______/______/______/______/______/[TomekK]
 *******************************************************************************
       ''')
-print("\n")
-print("Welcome to the Treasure Island!\nYou are at a crossroad.Where do you want to go?")
-Direction = input("Type left or right\n").lower()
-if Direction == "right":
-    print("You're arrived near a lake,There's an island in middle of the lake.\nWould you like to wait for a boat or swim across.")
-    Swim = input("Chosse wait or swim\n").lower()
-    if Swim == "wait":
-        print("You've successfully arrived at the island.\nThere are three doors in front of you.")
-        Door = input("Choose a door to go through.red,blue or yellow.\n").lower()
-        if Door == "red" or Door == "blue" :
-            print("You got eaten by a monster.Game over.")
-        else:
-            print("Congratulations! You got the treasure.")
-    else:
-        print("You got eaten by a Crocodile.Game over.")
-            
 
-else: 
-    print("You fell in a pit.Game over")    
+print("\n")
+print("Welcome to Treasure Island!")
+print("You are at a crossroad. Where do you want to go?")
+
+direction = input("Type left or right\n").strip().lower()
+
+if direction == "right":
+    print(
+        "You've arrived near a lake. "
+        "There's an island in the middle of the lake.\n"
+        "Would you like to wait for a boat or swim across?"
+    )
+
+    swim = input("Choose wait or swim\n").strip().lower()
+
+    if swim == "wait":
+        print(
+            "You've successfully arrived at the island.\n"
+            "There are three doors in front of you."
+        )
+
+        door = input(
+            "Choose a door to go through: red, blue or yellow.\n"
+        ).strip().lower()
+
+        if door == "yellow":
+            print("Congratulations! You got the treasure.")
+        elif door == "red" or door == "blue":
+            print("You got eaten by a monster. Game over.")
+        else:
+            print("Invalid choice. Please choose red, blue, or yellow.")
+
+    else:
+        print("You got eaten by a crocodile. Game over.")
+
+else:
+    print("You fell in a pit. Game over.")
